83fe132daef9a201d5d0654c2b0af448d1d7711d
Pfeifle, Brian <brian.pfeifle@mines.sdsmt.edu>
2022-11-09 18:01:44 -0700

    Working on setting up Staff create page

--- app/controllers/staff/staffs_controller.rb
+++ app/controllers/staff/staffs_controller.rb
@@ -1,5 +1,21 @@
 class Staff::StaffsController < ApplicationController
   def view
-    @staff = Staff.all
+    @staff = ::Staff.all
+  end
+
+  def new
+    @staff = ::Staff.new
+  end
+
+  def create
+    @staff = ::Staff.new(params[:staff])
+
+    if @staff.save
+      flash[:notice] = 'Staff was successfully created.'
+      redirect_to staff_staffs_view_path
+    else
+      flash[:notice] = 'Staff was unsuccessfully created'
+      redirect_to staff_staffs_view_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   namespace :staff do
     get 'staffs/view'
+    get 'staffs/new'
+    get 'staffs/create'
   end
   namespace :controllers do
     namespace :staff do
-      get 'staffs/viewStaff'
-    end
+        get 'staffs/viewStaff'
+        get 'staffs/new'
+        post 'staffs/create'
+      end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #root 'pages#index'
   get 'pages/index', to:'pages#index'
-  get 'staff/staffs/view', to:'staffs#viewStaff'
-  # Defines the root path route ("/")
+  get 'staff/staffs/view', to:'staffs#View'
+  get 'staff/staffs/new', to: 'staffs#New'
+  post 'staff/staffs/new', to: 'staffs#create'
+
   # root "articles#index"
 end
