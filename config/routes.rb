246d839dca8e53cdb9b51d5ad02b9dd2741960b0
Pfeifle, Brian <brian.pfeifle@mines.sdsmt.edu>
2022-11-16 14:20:27 -0700

    CRUD operations, minus delete, are working for the staff

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,10 @@
 Rails.application.routes.draw do
-  namespace :staff do
-    get 'staffs/view'
-    get 'staffs/new'
-    get 'staffs/create'
-  end
-  namespace :controllers do
-    namespace :staff do
-        get 'staffs/viewStaff'
-        get 'staffs/new'
-        post 'staffs/create'
-      end
-  end
+  get 'staffs/index', to: 'staffs#index'
+  resources :staffs
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #root 'pages#index'
   get 'pages/index', to:'pages#index'
-  get 'staff/staffs/view', to:'staffs#View'
-  get 'staff/staffs/new', to: 'staffs#New'
-  post 'staff/staffs/new', to: 'staffs#create'
 
   # root "articles#index"
 end
--- test/controllers/controllers/staff/staffs_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Controllers::Staff::StaffsControllerTest < ActionDispatch::IntegrationTest
-  test "should get viewStaff" do
-    get controllers_staff_staffs_viewStaff_url
-    assert_response :success
-  end
-end
