1649056a0d8ce689889fe8cee3e6ddf838e72a8a
Noah Johnson <Noah.Johnson@mines.sdsmt.edu>
2022-12-13 20:39:58 -0700

    Fixed CRUD for all 4 categories

--- db/migrate/20221031170617_create_orders.rb
+++ db/migrate/20221031170617_create_orders.rb
@@ -1,4 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
+  def up
+    change_column :orders, :order_id, auto_increment: true
+  end
+
   def change
     create_table :orders, id: false do |t|
       t.string :id, primary_key: true
